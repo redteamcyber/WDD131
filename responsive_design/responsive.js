fbf58c0d04fee4725f053c3b6aab50fa581fdcfa
redteamcyber <jen22034@byui.edu>
2025-10-08 18:42:41 -0600

    w04 ponder responsive

---
+++ responsive_design/responsive.js
@@ -0,0 +1,8 @@
+
+const menuBtn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('hidden');
+  menuBtn.classList.toggle('active');
+});
