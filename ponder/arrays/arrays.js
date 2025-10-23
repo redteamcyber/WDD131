8be495234e58834f398c251ea1e807cf5668beb0
redteamcyber <jen22034@byui.edu>
2025-10-23 16:44:54 -0600

    array practice complete

--- ponder/arrays/arrays.js
+++ ponder/arrays/arrays.js
@@ -1,3 +1,4 @@
+// map 1
 const steps = ["one", "two", "three"];
 function listTemplate(step){
     return `<li>${step}</li>`;
@@ -5,3 +6,31 @@ function listTemplate(step){
 
 const stepsHtml = steps.map(listTemplate);
 document.querySelector("#myList").innerHTML = stepsHtml.join("");
+
+// map 2
+const grades = ["A", "B", "A"];
+function convertGradeToPoints(grade){
+    let points = 0;
+    if (grade === "A"){
+        points = 4;
+    } else if (grade === "B"){
+        points = 3;
+    return points;
+}
+}
+
+// reduce
+
+const gpaPoints = grades.map(convertGradeToPoints);
+const pointsTotal = gpaPoints.reduce(function(total, item) {
+    return total + item;
+});
+
+const gpa = pointsTotal / gpaPoints.length;
+
+// filter
+
+const fruits = ["watermelon", "peach", "apple", "tomato", "grape"]
+constLength = words.filter(function(word) {
+    return word.length < 6;
+});
